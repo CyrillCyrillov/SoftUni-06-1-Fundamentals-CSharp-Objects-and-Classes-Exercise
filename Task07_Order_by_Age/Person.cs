75f7d32b872210c46023eb0c0da8e1b2c97797f4
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-18 23:07:00 +0200

    Add files via upload

---
+++ Task07_Order_by_Age/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Task07_Order_by_Age
+{
+    class Person
+    {
+
+
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Task07_Order_by_Age/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task07_Order_by_Age
+{
+    class Person
+    {
+       
+
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"{Name} with ID: {ID} is {Age} years old.";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if(data[0].ToUpper() == "END")
+                {
+                    break;
+                }
+
+                new Person = 
+
+
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
